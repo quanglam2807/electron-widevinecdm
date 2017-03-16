e75db20b12bf5b5b71c30c8ac6bd2f3050e9365f
Quang Lam <quang.lam2807@gmail.com>
2017-03-16 16:35:27 -0500

    Add AppVeyor

--- src/index.js
+++ src/index.js
@@ -8,41 +8,56 @@ const WIDEVINECDM_VERSION = process.env.npm_package_version;
 
 console.log(`Platform: ${process.platform}`);
 
-// https://electron.atom.io/docs/tutorial/using-widevine-cdm-plugin/
-if (process.platform === 'darwin') {
-  const pluginPath = `/Applications/Google Chrome.app/Contents/Versions/${CHROME_VERSION}/Google Chrome Framework.framework/Libraries/WidevineCdm`;
-  if (fs.existsSync(pluginPath)) {
-    console.log('Plugin exists.');
-    fsp.readFile(`${pluginPath}/manifest.json`)
-      .then((manifestJSON) => {
-        const info = JSON.parse(manifestJSON);
-        if (info.version !== WIDEVINECDM_VERSION) {
-          console.log(`npm version: ${WIDEVINECDM_VERSION}`);
-          console.log(`Manifest version: ${info.version}`);
-          return Promise.reject(new Error('Manifest version doesn\'t match.'));
-        }
-        return null;
-      })
-      .catch((err) => {
-        console.log(err);
-        process.exit(1);
-      });
-  } else {
-    process.exit(1);
-    console.log('Plugin does not exist.');
+let pluginPaths;
+switch (process.platform) {
+  case 'linux': {
+    pluginPaths = [
+      '/opt/google/chrome/libwidevinecdm.so',
+      '/opt/google/chrome/libwidevinecdmadapter.so',
+    ];
+    break;
+  }
+  case 'win32': {
+    pluginPaths = [
+      `${process.env.LOCALAPPDATA}/Google/Chrome/WidevineCDM/${WIDEVINECDM_VERSION}/_platform_specific/win_x64/widevinecdm.dll`,
+      `${process.env.LOCALAPPDATA}/Google/Chrome/WidevineCDM/${WIDEVINECDM_VERSION}/_platform_specific/win_x64/widevinecdmadapter.dll`,
+    ];
+    break;
+  }
+  default: { // darwin
+    pluginPaths = [
+      `/Applications/Google Chrome.app/Contents/Versions/${CHROME_VERSION}/Google Chrome Framework.framework/Libraries/WidevineCdm/_platform_specific/mac_x64/libwidevinecdm.dylib`,
+      `/A'pplications/Google Chrome.app/Contents/Versions/${CHROME_VERSION}/Google Chrome Framework.framework/Libraries/WidevineCdm/_platform_specific/mac_x64/widevinecdmadapter.plugin`,
+    ];
   }
 }
 
-if (process.platform === 'linux') {
-  const pluginPaths = [
-    '/opt/google/chrome/libwidevinecdm.so',
-    '/opt/google/chrome/libwidevinecdmadapter.so',
-  ];
+// https://electron.atom.io/docs/tutorial/using-widevine-cdm-plugin/
 
-  if (fs.existsSync(pluginPaths[0]) && fs.existsSync(pluginPaths[1])) {
-    console.log('Plugin exists.');
-  } else {
-    process.exit(1);
-    console.log('Plugin does not exist.');
-  }
+if (fs.existsSync(pluginPaths[0]) && fs.existsSync(pluginPaths[1])) {
+  console.log('Plugin exists.');
+
+  Promise.as()
+    .then(() => {
+      if (process.platform === 'darwin') {
+        return fsp.readFile(`/Applications/Google Chrome.app/Contents/Versions/${CHROME_VERSION}/Google Chrome Framework.framework/Libraries/WidevineCdm/manifest.json`)
+          .then((manifestJSON) => {
+            const info = JSON.parse(manifestJSON);
+            if (info.version !== WIDEVINECDM_VERSION) {
+              console.log(`npm version: ${WIDEVINECDM_VERSION}`);
+              console.log(`Manifest version: ${info.version}`);
+              return Promise.reject(new Error('Manifest version doesn\'t match.'));
+            }
+            return null;
+          });
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+} else {
+  process.exit(1);
+  console.log('Plugin does not exist.');
 }
