85ea2847235753fa8b24648c11bbc15ab5c6e6bf
Quang Lam <quang.lam2807@gmail.com>
2017-03-16 23:34:44 -0500

    load script

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const WIDEVINECDM_VERSION = require('../package').version;
+
+const load = (app) => {
+  let widevineCdmPluginFilename;
+  switch (process.platform) {
+    case 'darwin':
+      widevineCdmPluginFilename = 'widevinecdmadapter.plugin';
+      break;
+    case 'linux':
+      widevineCdmPluginFilename = 'libwidevinecdmadapter.so';
+      break;
+    default:
+    case 'win32':
+      widevineCdmPluginFilename = 'widevinecdmadapter.dll';
+  }
+
+  app.commandLine.appendSwitch('widevine-cdm-path', path.join(__dirname, '..', 'dist', widevineCdmPluginFilename));
+  app.commandLine.appendSwitch('widevine-cdm-version', WIDEVINECDM_VERSION);
+};
+
+module.exports = { load };
